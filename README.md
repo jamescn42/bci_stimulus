c4cdd4fdf729ed61d69dae81cd371f15fd6fa6c4
James Chen <james.chen3@ucalgary.ca>
2020-05-15 15:27:39 -0600

    Added GUI)

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # BCI SSVEP Stimulus
 Author: James Chen
 
-Python script controls Arduino MEGA to flash 4 LED's at frequencies between 1 and 999
+Python script opens a GUI that controls Arduino MEGA to flash 4 groups of LED's at integer frequencies between 1Hz and 100Hz.
+
 Note: Acutal Frequency will be sleightly lower then desired. Around 99% accuracy
 
- 
-
 # How to run this program
-Upload `pi_to_arduino.ino` to arduino MEGA and connect 4 LED's as well as resistors to corrosponding pins (2,3,4,5), see wiring diagram. Run `set_frequencies.py` on Raspberry Pi with USB cable (same one to program Arduino). To change frequencies, change values on line 24, in `set_frequencies.py` to desired frequencies. 
+Upload `pi_to_arduino.ino` to arduino MEGA and connect LED's as well as resistors to corrosponding pins (2,3,4,5 by default), see wiring diagram. Run `set_frequencies.py` on Raspberry Pi with USB cable (same one to program Arduino). This will open a GUI to set the desired LED frequencies.
 
+Frequency Setting GUI:
+![Screenshot of example GUI](gui_screenshot.png)
 
 Wiring Diagram:
 `to come!`
--- set_frequencies.py
+++ set_frequencies.py
@@ -1,25 +1,77 @@
-#Author: James Chen
-#University of Calgary
+# Author: James Chen
+# University of Calgary
+
 import serial
-        
-def set_freq(freq1, freq2, freq3, freq4):
-    """Sets the frequency what the lights flash at
-    freq<numb> (int): a positive 3 digit integer frequency 
-    
-    
-    """
-    if not (0<freq1<999 or 0<freq2<999 or 0<freq3<999 or 0<freq4<999):
-        print("That frequency is out of bounds! Please choose a diffrent frequency (0<freq<999)")
-        return
-    
-    ser.flush()
-    message = f'{freq1:03}'+','+f'{freq2:03}'+','+f'{freq3:03}'+','+f'{freq4:03}'+','
-    ser.write(message.encode('utf-8'))
-    
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import simpledialog
+
+
+class FrequencyConfig(ttk.Frame):
+    def __init__(self, parent=None):
+        super().__init__(parent, padding=5)
+        self.grid(sticky=tk.N+tk.S+tk.W+tk.E)
+        self.makewidgets()
+        self.freqs = [1, 1, 1, 1]
+        self.boxes = []
+
+    def makewidgets(self):
+        # title label
+        ttk.Label(self, text='Set Frequencies for SSVEP',
+                  font=("Verdana", 16)).grid(column=0, row=0, columnspan=2)
+
+        self.freqs = [1, 1, 1, 1]
+        self.boxes = []
+        # frequency 1
+        ttk.Label(self, text='Frequency for LED 1 (Hz):').grid(
+            column=0, row=1)
+        self.boxes.append(tk.Text(self, width=15, height=1))
+        self.boxes[0].grid(column=1, row=1, sticky='W')
+
+        # frequency 2
+        ttk.Label(self, text='Frequency for LED 2 (Hz):').grid(
+            column=0, row=2)
+        self.boxes.append(tk.Text(self, width=15, height=1))
+        self.boxes[1].grid(column=1, row=2, sticky='W')
+
+        # frequency 3
+        ttk.Label(self, text='Frequency for LED 3 (Hz):').grid(
+            column=0, row=3)
+        self.boxes.append(tk.Text(self, width=15, height=1))
+        self.boxes[2].grid(column=1, row=3, sticky='W')
+
+        # frequency 4
+        ttk.Label(self, text='Frequency for LED 4 (Hz):').grid(
+            column=0, row=4)
+        self.boxes.append(tk.Text(self, width=15, height=1))
+        self.boxes[3].grid(column=1, row=4, sticky='W')
+
+        # set frequencies button
+        ttk.Button(self, text='Set Frequencies', command=self.set_freq).grid(
+            column=0, row=5, columnspan=2)
+
+    def set_freq(self):
+        for i in range(4):
+            try:
+                if(0 < int(self.boxes[i].get('1.0', tk.END+'-1c')) <= 100):
+                    self.freqs[i] = int(self.boxes[i].get('1.0', tk.END+'-1c'))
+                else:
+                    messagebox.showinfo(
+                        "Error", "That frequency is out of bounds! Please choose a diffrent frequency (0<freq<=100)")
+            except:
+                pass
+
+        ser.flush()
+        message = f'{self.freqs[0]:03}'+','+f'{self.freqs[1]:03}' + \
+            ','+f'{self.freqs[2]:03}'+','+f'{self.freqs[3]:03}'+','
+        ser.write(message.encode('utf-8'))
+
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
-    
-    #default
-    set_freq(13, 17, 19, 23)
+
+    root = tk.Tk()
+    FrequencyConfig(root)
+    root.mainloop()
