a6dccec9aea3857fccab8ab2ccf10763231c7204
jamescn42 <54485074+jamescn42@users.noreply.github.com>
2020-06-10 17:08:05 -0600

    Update keyboard control image, add diagonal cases on Arduino

--- arduino_bci/a1_motor_control.ino
+++ arduino_bci/a1_motor_control.ino
@@ -11,26 +11,46 @@ void drive_motor(String data) {
   //TODO: code for output of direction of control.
   //      dependent on type of motor controller/wheelchair output
 
-  char direc = data[2];
+  char direc[] = {data[2], data[3], '\0'};
   switch (direc) {
-    case 'f':
+    case "nn":
       if(ultrasonic_distance[0] > 30 && ultrasonic_distance[4] < 30){
-        //code for forward direction
+        //code for n direction
       }
       break;
-    case 'b':
+    case "ww":
       if (ultrasonic_distance[3] >30){
-        //code for back direction
+        //code for w direction
       }
       break;
-    case 'l':
+    case "ss":
       if (ultrasonic_distance[1] >30){
-        //code for left direction
+        //code for s direction
       }
       break;
-    case 'r':
+    case "ee":
       if (ultrasonic_distance[2] >30){
-        //code for right direction
+        //code for e direction
+      }
+      break;
+    case "nw":
+      if (ultrasonic_distance[0] > 30 && ultrasonic_distance[4] < 30 && ultrasonic distance[2] >30){
+        //code for nw direction
+      }
+      break;
+    case "ne":
+      if (ultrasonic_distance[0] > 30 && ultrasonic_distance[4] < 30 && ultrasonic distance[3] >30){
+        //code for ne direction
+      }
+      break;
+    case "se":
+      if (ultrasonic_distance[1] >30){
+        //code for se direction
+      }
+      break;
+    case "sw":
+      if (ultrasonic_distance[1] >30){
+        //code for sw direction
       }
       break;
   }
